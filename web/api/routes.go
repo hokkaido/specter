b5b1cd26ca1bd5294da1e789b92e80e415207680
hokkaido <hello@kimchi.io>
2014-11-27 16:41:18 +0100

    fix assignments

--- web/api/routes.go
+++ web/api/routes.go
@@ -10,8 +10,8 @@ func RegisterRoutes(r *mux.Router, dockerAddr string) {
 	apiRouter := mux.NewRouter()
 	container.RegisterRoutes(apiRouter)
 
-	apiWithMiddleware = negroni.New()
-	apiMiddleware = NewApiMiddleware(dockerAddr)
+	apiWithMiddleware := negroni.New()
+	apiMiddleware := NewApiMiddleware(dockerAddr)
 	apiWithMiddleware.Use(negroni.HandlerFunc(apiMiddleware.ServeHTTP))
 	apiWithMiddleWare.UseHandler(apiRouter)
 	r.Handle("/api/", apiWithMiddleware)
