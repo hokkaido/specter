ffbad87337bf76554b952feb2114614e060b767f
kitsune <hello@kimchi.io>
2014-12-01 11:13:01 +0100

    Removal of angular-material, added ZURB foundation

--- web/static/app/core/core.module.js
+++ web/static/app/core/core.module.js
@@ -5,7 +5,7 @@
         /*
          * Angular modules
          */
-        'ngAnimate', 'ngRoute', 'ngSanitize', 'ngMaterial'
+        'ngAnimate', 'ngRoute', 'ngSanitize', 
         /*
          * Our reusable cross app code modules
          */
